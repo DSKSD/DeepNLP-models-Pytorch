f96fe3db70d7118ab0c85dea5ab9cde2c73c20e5
dsksd <tjdehd1222@gmail.com>
2017-10-08 13:29:56 +0900

    nmt

--- Corpus.pyx
+++ Corpus.pyx
@@ -26,3 +26,26 @@ def make_coo_matrix(corpus,dictionary):
     pass
     
 
+def getBatch_FromBucket(batch_size,buckets):
+    i=0
+    bucket_mask =[False for _ in range(len(buckets))]
+    indices = [[0,batch_size] for _ in range(len(buckets))]
+    is_done=False
+    while is_done==False:
+        batch = buckets[i][indices[i][0]:indices[i][1]]
+        temp = indices[i][1]
+        indices[i][1]= indices[i][1]+batch_size
+        indices[i][0] = temp
+        
+        i = (i+1)%len(buckets)
+        while bucket_mask[i]:
+            i = (i+1)%len(buckets)
+        
+        if indices[i][1]>len(buckets[i]):
+            bucket_mask[i]= True
+            if bucket_mask.count(True)==len(buckets):
+                is_done=True
+            else:
+                while bucket_mask[i]:
+                    i = (i+1)%len(buckets)
+        yield batch
